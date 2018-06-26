6127894559890ab454ca41b9baf9380c75d89fda
cailufei <cailufei@xqshijie.cn>
2018-06-26 13:23:28 +0800

    no message

--- MSPayThrough/MSPayThrough/Other/json/resp/ShareImgModel.java
+++
@@ -1,8 +0,0 @@
-package com.tendpay.business.json.resp;
-
-import java.io.Serializable;
-
-public class ShareImgModel implements Serializable{
-	public String sms_image_url;
-	public String sm_register_url;
-}

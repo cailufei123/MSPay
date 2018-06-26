e3ad3396b16db606f2c3dde9ac8b70152c7cb0e5
cailufei <cailufei@xqshijie.cn>
2018-06-26 14:18:29 +0800

    no message

---
+++ MSPayThrough/MSPayThrough/MSMe/MSMeController/MSMeViewController.m
@@ -0,0 +1,48 @@
+//
+//  MSMeViewController.m
+//  MSPayThrough
+//
+//  Created by 蔡路飞 on 2018/6/25.
+//  Copyright © 2018年 蔡路飞. All rights reserved.
+//
+
+#import "MSMeViewController.h"
+
+@interface MSMeViewController ()
+
+@end
+
+@implementation MSMeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"111");
+}
+
+//点击头像
+- (IBAction)clicTopBtn {
+}
+//点击费率
+- (IBAction)clickFeiLvBtn {
+}
+//点击银行卡
+- (IBAction)clickBankBtn {
+}
+//点击公告
+- (IBAction)clickGongGaoBtn {
+}
+//点击分享
+- (IBAction)clickShareBtn {
+}
+//点击会员
+- (IBAction)clickVipBtn {
+}
+//点击客服
+- (IBAction)clickKeFuBtn {
+}
+//点击设置
+- (IBAction)clickSetBtn {
+}
+
+@end

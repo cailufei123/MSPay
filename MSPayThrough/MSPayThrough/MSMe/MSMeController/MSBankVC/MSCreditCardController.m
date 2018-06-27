82a54d99fd375fe2143592700966ea4e56c928d4
ribencun <570703516@qq.com>
2018-06-27 18:43:16 +0800

    添加银行卡界面

--- MSPayThrough/MSPayThrough/MSMe/MSMeController/MSBankVC/MSCreditCardController.m
+++ MSPayThrough/MSPayThrough/MSMe/MSMeController/MSBankVC/MSCreditCardController.m
@@ -20,6 +20,8 @@
     
     //初始化tableview
     [self setUpTableView];
+    //初始化view
+    [self setUpView];
 }
 
 - (void)setUpTableView{
@@ -33,6 +35,13 @@
 //    tableView.tableFooterView = [[UIView alloc] init];
 //    [self.view addSubview:tableView];
 //    self.tableView = tableView;
+}
+
+- (void)setUpView{
+    UIButton *addButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    addButton.frame = CGRectMake(45, LFscreenH - 20, LFscreenW - 90, 50);
+    [addButton gradientFreme: CGRectMake(0, 0, LFscreenW - 90, 50) startColor:[SVGloble colorWithHexString:@"#ef6468"] endColor:[SVGloble colorWithHexString:@"#713d92"]];
+    [self.view addSubview:addButton];
 }
 
 @end

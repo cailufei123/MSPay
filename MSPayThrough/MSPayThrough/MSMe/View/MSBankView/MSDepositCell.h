76f0c024ec8a9a665f7b17e588f9f213a6ae09db
ribencun <570703516@qq.com>
2018-06-27 19:45:04 +0800

    增加银行卡界面

---
+++ MSPayThrough/MSPayThrough/MSMe/View/MSBankView/MSDepositCell.h
@@ -0,0 +1,13 @@
+//
+//  MSDepositCell.h
+//  MSPayThrough
+//
+//  Created by ribencun on 2018/6/27.
+//  Copyright © 2018年 蔡路飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSDepositCell : UITableViewCell
+
+@end

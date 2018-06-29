10f82574541057abf48622f2448a78b36b3c9290
ribencun <570703516@qq.com>
2018-06-29 20:43:27 +0800

    百度SDK

--- MSPayThrough/MSPayThrough/MSMe/MSMeController/MSBankVC/MSAddDepositCardController.m
+++ MSPayThrough/MSPayThrough/MSMe/MSMeController/MSBankVC/MSAddDepositCardController.m
@@ -50,15 +50,32 @@
 }
 //点击相机
 - (IBAction)clickCameraBtn {
+//    UIViewController * vc =
+//    [AipCaptureCardVC ViewControllerWithCardType:CardTypeBankCard
+//                                 andImageHandler:^(UIImage *image) {
+//
+//                                     [[AipOcrService shardService] detectBankCardFromImage:image
+//                                                                            successHandler:_successHandler
+//                                                                               failHandler:_failHandler];
+//
+//                                 }];
+//    [self presentViewController:vc animated:YES completion:nil];
     UIViewController * vc =
-    [AipCaptureCardVC ViewControllerWithCardType:CardTypeBankCard
-                                 andImageHandler:^(UIImage *image) {
-                                     
-                                     [[AipOcrService shardService] detectBankCardFromImage:image
-                                                                            successHandler:_successHandler
-                                                                               failHandler:_failHandler];
-                                     
-                                 }];
+    [AipCaptureCardVC ViewControllerWithCardType:CardTypeLocalIdCardFont andImageHandler:^(UIImage *image) {
+        // 成功扫描出身份证
+        [[AipOcrService shardService] detectIdCardFrontFromImage:image
+                                                     withOptions:nil
+                                                  successHandler:^(id result){
+                                                      // 在成功回调中，保存图片到系统相册
+                                                      UIImageWriteToSavedPhotosAlbum(image, nil, nil, (__bridge void *)self);
+                                                      // 打印出识别结果
+                                                      NSLog(@"%@", result);
+                                                      
+                                                  }
+         
+                                                     failHandler:_failHandler];
+    }];
+    // 展示ViewController
     [self presentViewController:vc animated:YES completion:nil];
 
 }
